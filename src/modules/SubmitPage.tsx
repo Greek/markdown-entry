a21c4b3b4a037de1814d758ed52bbda4d3b55325
Andreas <me@apap04.com>
2022-08-14 07:24:06 -0400

    refactor: Make this cool
    Signed-off-by: Andreas <me@apap04.com>

--- src/modules/SubmitPage.tsx
+++ src/modules/SubmitPage.tsx
@@ -2,6 +2,7 @@ import { CenterLayout } from '../ui/layouts/CenterLayout';
 import { Button } from '../ui/Button';
 import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
+import { TextArea } from '../ui/TextArea';
 
 export const SubmitPage = ({ editMode = false, ...props }) => {
   const textAreaRef = useRef(null);
@@ -15,8 +16,6 @@ export const SubmitPage = ({ editMode = false, ...props }) => {
   useEffect(() => {
     setTextEditor(props.entry?.content);
   }, [editMode, props.entry]);
-
-  console.log(props);
 
   const performSaveAction = () => {
     fetch(`/api`, {
@@ -59,41 +58,44 @@ export const SubmitPage = ({ editMode = false, ...props }) => {
         editCode: editCodeTextAreaRef?.current?.value as unknown,
       }),
     }).then(async (r) => {
+      const res = await r.json();
+
       if (!r.ok) {
-        setErrorMessage('Please provide content.');
+        setErrorMessage(res.error);
         return;
       }
 
-      const res = await r.json();
       push(`/${res.id}?c=${res.editCode}`, `/${res.id}`);
     });
   };
 
   return (
-    <CenterLayout>
-      <div className={`flex flex-col py-3`}>
+    <>
+      <div className={`flex flex-col mx-96 pt-3`}>
         <textarea
-          className={`bg-[#464646] text-[#999] p-4 outline-none`}
+          className={`bg-button-bg text-color-text p-4 mb-2 outline-none h-96`}
           ref={textAreaRef}
           defaultValue={textEditor}
-        ></textarea>
-      </div>
-      {editMode && (
-        <textarea
-          className={`bg-button-bg text-color-text p-4 outline-none ${
-            errorMessage && 'border-red-500 border-l-4'
-          }`}
-          ref={editCodeTextAreaRef}
-          placeholder="Edit code"
-        ></textarea>
-      )}
-      {editMode ? (
-        <Button onClick={performEditAction}>Submit edit</Button>
-      ) : (
-        <Button onClick={performSaveAction}>Submit</Button>
-      )}
+        >
+          {props.children}
+        </textarea>
+        {editMode && (
+          <input
+            className={`bg-button-bg text-color-text max-w-xs p-4 outline-none resize-none ${
+              errorMessage && 'border-red-500 border-l-4'
+            }`}
+            ref={editCodeTextAreaRef}
+            placeholder="Edit code"
+          ></input>
+        )}
+        {editMode ? (
+          <Button onClick={performEditAction}>Submit edit</Button>
+        ) : (
+          <Button onClick={performSaveAction}>Submit</Button>
+        )}
 
-      <p>{errorMessage}</p>
-    </CenterLayout>
+        <p>{errorMessage}</p>
+      </div>
+    </>
   );
 };
