7c145d870ad8b7bca8df111f5cc1ddda34c1aad6
Andreas <me@apap04.com>
2022-08-29 23:53:59 -0400

    fix: break text if too long
    Signed-off-by: Andreas <me@apap04.com>

--- src/ui/TextBox.tsx
+++ src/ui/TextBox.tsx
@@ -3,7 +3,7 @@
 export const TextBox2 = ({ ...props }) => {
   return (
     <div
-      className={`flex flex-col py-4 px-4 bg-button-bg text-color-text min-h-[48rem] mb-4 min-w-full overflow-y-auto`}
+      className={`flex flex-col py-4 px-4 bg-button-bg text-color-text min-h-[48rem] mb-4 min-w-full overflow-y-auto break-words`}
     >
       {props.children}
     </div>
