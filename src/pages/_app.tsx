1a4ffe3ff4be8718e2242977dd8512ee3cfc212d
Andreas <me@apap04.com>
2022-10-15 20:58:06 -0400

    idk
    Signed-off-by: Andreas <me@apap04.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,11 @@
-import { NextPageContext } from 'next';
-import { getSession, SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 
 import '../styles/global.scss';
 
-export const getServerSideProps = async (context: NextPageContext) => {
-  return {
-    props: {
-      session: await getSession(context),
-    },
-  };
-};
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <SessionProvider session={pageProps.session} refetchInterval={0}>
-        <Component {...pageProps} />
-      </SessionProvider>
+      <Component {...pageProps} />
     </>
   );
 }
